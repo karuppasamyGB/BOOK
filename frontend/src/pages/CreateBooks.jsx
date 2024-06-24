03d87d8fef3ef0d6ea8b3851f819bb5a7d8ee528
karuppasamyGB <148546076+karuppasamyGB@users.noreply.github.com>
2024-06-24 16:52:39 +0530

    Update CreateBooks.jsx

--- frontend/src/pages/CreateBooks.jsx
+++ frontend/src/pages/CreateBooks.jsx
@@ -18,7 +18,7 @@ const CreateBooks = () => {
       publishYear,
     };
     setLoading(true);
-    axios.post('http://localhost:5555/books', data)
+    axios.post('https://book-backend-mu-ten.vercel.app/books', data)
       .then(() => {
         setLoading(false);
         enqueueSnackbar('Book Created Succcessfully',{variant: 'success'});

1403001828e762105cde9b200493e855f974907e
karuppasamyGB <148546076+karuppasamyGB@users.noreply.github.com>
2024-06-24 16:54:29 +0530

    Update ShowBook.jsx

--- frontend/src/pages/ShowBook.jsx
+++ frontend/src/pages/ShowBook.jsx
@@ -12,7 +12,7 @@ const ShowBook = () => {
     useEffect(() => {
         setLoading(true);
         axios
-            .get(`http://localhost:5555/books/${id}`)
+            .get(`https://book-backend-mu-ten.vercel.app/books/${id}`)
             .then((response) => {
                 setBook(response.data);
                 setLoading(false);

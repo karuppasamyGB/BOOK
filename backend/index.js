65a6a8e820cbbc2868daad3938f46570775d9125
karuppasamyGB <148546076+karuppasamyGB@users.noreply.github.com>
2024-06-24 16:50:37 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "https://book-frontend-vert.vercel.app/",
     methods: ["GET", "POST", "PUT", "DELETE"],
 
   }));

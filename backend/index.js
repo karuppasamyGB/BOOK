b1c490b636905ac5b9202862449bb4032948d109
karuppasamyGB <148546076+karuppasamyGB@users.noreply.github.com>
2024-07-07 17:10:07 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { PORT, mongoDBURL } from "./config.js";
+import { mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import bookRoute from "./routes/bookRoute.js";
 import cors from "cors";

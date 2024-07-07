4ff842c61affb9be05e6c7b9df28feacbf7ffb57
karuppasamyGB <148546076+karuppasamyGB@users.noreply.github.com>
2024-07-07 16:39:09 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -18,7 +18,7 @@ const Home = () => {
     useEffect(() => {
         setLoading(true);
         axios
-            .get('https://book-backend-05m5.onrender.com/')
+            .get('https://book-backend-05m5.onrender.com/books')
             .then((response) => {
                 setBooks(response.data.data);
                 setLoading(false);
